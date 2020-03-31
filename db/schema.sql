90d7695ebeb65ca72ced1027b43904224fd75ce7
mjimenez05 <matthewjjimenez05@hotmail.com>
2020-03-31 17:41:04 -0600

    Added orm file, put in my connection file and neccisary code. Need to debug to run the initial app.

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,7 @@ USE burger_db:
 
 CREATE TABLE burgers
 (
-    id int NOT NULL AUTO_INCRIMENT,
+    id int NOT NULL AUTO_INCREMENT,
     name varchar(255) NOT NULL,
     devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (name, devoured) VALUES ('Blue Cheese Borg', true);
+INSERT INTO burgers (name, devoured) VALUES ('Fungus Among-us Borg', true);
+INSERT INTO burgers (name, devoured) VALUES ('Southern Borg', true);
+INSERT INTO burgers (name) VALUES ('Plain Jane Borg');
