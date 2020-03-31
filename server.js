323c73c3fbfa026ceff9044bf44cb7ca443b34b7
mjimenez05 <matthewjjimenez05@hotmail.com>
2020-03-31 16:40:05 -0600

    Made the schema for the db, Created the express require on server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require("express");
+
+var PORT = process.env.PORT || 3306;
+
+var app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencodded({ extended: true }));
+app.use(express.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("veir engine", "handlebars");
+
+var routes = require("./controllers/burgerController.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("App now listening on localhost:" + PORT)
+});
